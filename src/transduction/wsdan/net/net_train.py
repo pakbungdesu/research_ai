8bfd4aff3976dcdc4115e2c4cdcb670f2d34122b
pakbung2000 <pakbungteam7@gmail.com>
2026-06-29 12:30:12 +0700

    change theta

--- src/transduction/wsdan/net/net_train.py
+++ src/transduction/wsdan/net/net_train.py
@@ -81,8 +81,8 @@ def _train(device, logs, train_loader, net, feature_center, optimizer, pbar,
             crop_images, _ = batch_augment(X, paths, attention_map[:, :1, :, :],
                 savepath=savepath_batch,
                 use_doppler=with_doppler, config_doppler=config_doppler,
-                mode='crop', theta=(0.75, 0.95),   # Lower the minimum threshold to 0.75 for safety
-                padding_ratio=0.1)   # Use a small 10% safety margin to prevent 0-pixel collapses
+                mode='crop', theta=(0.7, 0.95),
+                padding_ratio=0.1)
 
         if savepath_batch:  # @@
             for idx in range(crop_images.shape[0]):
