5d5d249f553ceee725b8002cd1c5713275a184b9
pakbung2000 <pakbungteam7@gmail.com>
2026-06-29 15:04:56 +0700

    change padding back due to runtime disconnected

--- src/transduction/wsdan/net/net_train.py
+++ src/transduction/wsdan/net/net_train.py
@@ -82,7 +82,7 @@ def _train(device, logs, train_loader, net, feature_center, optimizer, pbar,
                 savepath=savepath_batch,
                 use_doppler=with_doppler, config_doppler=config_doppler,
                 mode='crop', theta=(0.7, 0.98),
-                padding_ratio=0.05)
+                padding_ratio=0.1)
 
         if savepath_batch:  # @@
             for idx in range(crop_images.shape[0]):
